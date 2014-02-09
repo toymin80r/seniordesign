93124bd3f1634af657098b585878ef3229792335
Jordan Rife <jrife0@gmail.com>
2014-02-08 20:11:26 -0600

    Adding unit tests and fixing bitstream code. Ready for networking API.

--- src/bitstream.js
+++ src/bitstream.js
@@ -1,31 +1,37 @@
-define(function() {
-    var BitStream = Class.create({
-        initialize: function() {
-            this._readIndex = 0;
-            this.reset();
-        },
+if(typeof define !== 'function') {
+    var define = require('amdefine')(module);
+}
+
+define(function(require) {
+    var BitStream = function() {
+        this.reset();
+    };
+    BitStream.prototype = {
         writeNumber: function(number) {
             this._buffer.push(number);
         },
         readNumber: function() {
-            return this._buffer[this._readIndex++];
+            return this._buffer.shift();
         },
         writeString: function(string) {
-            this._buffer.push(number);
+            this._buffer.push(string);
         },
         readString: function() {
-            return this._buffer[this._readIndex++];
+            return this._buffer.shift();
         },
         writeBool: function(boolean) {
-            this._buffer.push(number);
+            this._buffer.push(boolean);
         },
         readBool: function() {
-            return this._buffer[this._readIndex++];
+            return this._buffer.shift();
+        },
+        isEmpty: function() {
+            return this._buffer.length === 0;
         },
         reset: function() {
             this._buffer = [];
         }
-    });
+    };
 
     return BitStream;
 });
---
+++ test/bitstreamtest.js
@@ -0,0 +1,80 @@
+if(typeof define !== 'function') {
+    var define = require('amdefine')(module);
+}
+
+define(['../src/bitstream'], function(BitStream) {
+    function setUp(callback) {
+        this.stream = new BitStream();
+        callback();
+    }
+
+    function tearDown(callback) {
+        delete this.stream;
+        callback();
+    }
+
+    function testBitStreamResetAndIsEmpty(test) {
+        var stream = this.stream;
+
+        test.ok(stream.isEmpty());
+
+        stream.writeNumber(3);
+        stream.writeString("Hello");
+        stream.writeBool(true);
+
+        test.ok(!stream.isEmpty())
+
+        stream.reset();
+
+        test.ok(stream.isEmpty());
+
+        test.done();
+    }
+
+    function testWriteReadEmptinessCondition(test) {
+        var stream = this.stream;
+
+        stream.writeBool(true);
+        stream.writeString("Hello, World");
+        stream.writeNumber(249);
+
+        test.ok(!stream.isEmpty());
+
+        stream.readBool();
+        stream.readString();
+        stream.readNumber();
+
+        test.ok(stream.isEmpty());
+
+        test.done();
+    }
+
+    function testWriteReadOrderAndCorrectness(test) {
+        var stream = this.stream;
+
+        stream.writeBool(false);
+        stream.writeNumber(2329);
+        stream.writeNumber(92);
+        stream.writeString("Hello");
+        stream.writeNumber(2);
+        stream.writeString("Hi");
+
+        test.equal(stream.readBool(), false);
+        test.equal(stream.readNumber(), 2329);
+        test.equal(stream.readNumber(), 92);
+        test.equal(stream.readString(), "Hello");
+        test.equal(stream.readNumber(), 2);
+        test.equal(stream.readString(), "Hi");
+
+        test.done();
+    }
+
+    return {
+        setUp: setUp,
+        tearDown: tearDown,
+        testBitStreamResetAndIsEmpty: testBitStreamResetAndIsEmpty,
+        testWriteReadEmptinessCondition: testWriteReadEmptinessCondition,
+        testWriteReadOrderAndCorrectness: testWriteReadOrderAndCorrectness
+    }
+});
+
